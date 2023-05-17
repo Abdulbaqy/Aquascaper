10504d12a4fb3e58c68c779fcf5173154d7fa2d5
Abdulbaqy <baqy0306@gmail.com>
2023-05-17 09:31:00 +0700

    mulai dari awal aquascaper

---
+++ app/src/main/java/com/example/aquascaper/InvoiceForm.java
@@ -0,0 +1,14 @@
+package com.example.aquascaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvoiceForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_form);
+    }
+}
