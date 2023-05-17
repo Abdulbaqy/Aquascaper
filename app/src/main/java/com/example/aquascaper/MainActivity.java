44802a1adc4a2928473d60679e7ba6d678f7e46c
Rayhan Fahlevy <rayhanfahlevy@gmail.com>
2023-05-17 12:30:18 +0700

    Update Bottom Navigation Bar with Fragment nih ki.

--- app/src/main/java/com/example/aquascaper/MainActivity.java
+++ app/src/main/java/com/example/aquascaper/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.aquascaper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ChatFragment chatFragment = new ChatFragment();
+    ProjectFragment projectFragment = new ProjectFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.chat:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, chatFragment).commit();
+                        return true;
+                    case R.id.project:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, projectFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
